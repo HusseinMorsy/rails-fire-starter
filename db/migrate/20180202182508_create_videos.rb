486c19861827b919ec000a7437c44e93b68f4570
Hussein Morsy <hussein@morsy.de>
2018-02-02 19:34:05 +0100

    List videos

---
+++ db/migrate/20180202182508_create_videos.rb
@@ -0,0 +1,10 @@
+class CreateVideos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :videos do |t|
+      t.string :title
+      t.integer :duration
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_02_02_182508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "videos", force: :cascade do |t|
+    t.string "title"
+    t.integer "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
