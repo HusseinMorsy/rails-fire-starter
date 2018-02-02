a70dfa2cb45d02cda503833e166b74855927d7b3
Hussein Morsy <hussein@morsy.de>
2018-02-02 19:58:42 +0100

    Add flash messages

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -11,8 +11,10 @@ class VideosController < ApplicationController
     @video = Video.new(video_params)
     if @video.valid?
       @video.save!
+      flash[:notice] = "Video successfully created"
       redirect_to videos_url
     else
+      flash[:allert] = "Error"
       render :new
     end
   end
@@ -20,6 +22,7 @@ class VideosController < ApplicationController
   def destroy
     video = Video.find(params[:id])
     video.destroy!
+    flash[:notice] = "Video successfully deleted"
     redirect_to videos_url
   end
 
