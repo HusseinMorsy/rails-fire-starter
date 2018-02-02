105e02e47539961a414f53fc61280698e45d7f1b
Hussein Morsy <hussein@morsy.de>
2018-02-02 19:50:09 +0100

    Add new video form

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -4,4 +4,22 @@ class VideosController < ApplicationController
     @videos = Video.all
   end
 
+  def new
+  end
+
+  def create
+    @video = Video.new(video_params)
+    if @video.valid?
+      @video.save!
+      redirect_to videos_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def video_params
+    params.require(:video).permit(:title, :duration)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get '/videos', to: "videos#index"
+  get '/videos', to: 'videos#index'
+  get '/videos/new', to: 'videos#new', as: 'new_video'
+  post '/videos', to: 'videos#create'
 end
 
